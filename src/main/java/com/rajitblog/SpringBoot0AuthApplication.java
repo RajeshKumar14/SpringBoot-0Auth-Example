9537227ec5db14c5210a153b140a4c6c9b2a695d
Rajesh Kumar <kumarrajesh7671@gmail.com>
2024-02-23 08:57:50 +0530

    initial commit

---
+++ src/main/java/com/rajitblog/SpringBoot0AuthApplication.java
@@ -0,0 +1,13 @@
+package com.rajitblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot0AuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBoot0AuthApplication.class, args);
+	}
+
+}
